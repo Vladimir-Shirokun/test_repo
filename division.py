274a5f13fc0bcab13756ac6ae8596ccfc657e5a9
Широкун Владимир Владимирович <v.shirokun@Almaz.local>
2018-05-16 11:52:55 +0300

    changing2 in division.py

--- division.py
+++ division.py
@@ -7,9 +7,12 @@ if __name__ == '__main__':
     parser.add_argument('numerator', type=float, help='Numerator')
     parser.add_argument('divisor', type=float, help='Divisor')
     
+// one else commit
+
+
     args = parser.parse_args()
     if ( args.divisor == 0)
         print("Division on Null is incoreect")
     else
         print(args.numerator / args.divisor)
-
+// make comment
